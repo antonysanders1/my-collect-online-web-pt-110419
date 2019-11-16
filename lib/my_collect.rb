52fd5bbc6415071c9d49e3a781d8ce24fa97c7b7
Antony Sanders <antony.devon92@gmail.com>
2019-11-16 21:33:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,10 @@
 def my_collect(array)
     counter = 0
-    new_array = []
  
     while counter < array.length
       yield(array[counter])
       counter = counter + 1
-      new_array << array.split(" ").first
     end
-    new_array
+    array
 end
 
